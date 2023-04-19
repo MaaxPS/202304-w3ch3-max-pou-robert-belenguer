3d83f8523e67d442080fd17297291209e2531360
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-04-19 18:47:13 +0200

    Add component AppContainer

--- src/ts/components/Component/Component.ts
+++ src/ts/components/Component/Component.ts
@@ -1,13 +1,13 @@
 import { type ComponentStructure } from "../../types/types";
 
-abstract class Component implements ComponentStructure {
+export abstract class Component implements ComponentStructure {
   element: Element;
-
-  abstract renderHtml: () => void;
 
   constructor(parentElement: Element, tagName: string, className: string) {
     this.element = document.createElement(tagName);
     this.element.className = className;
     parentElement.appendChild(this.element);
   }
+
+  abstract renderHtml(): void;
 }

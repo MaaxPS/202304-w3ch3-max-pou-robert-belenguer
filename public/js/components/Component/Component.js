17dddc4a680fa53d714bbdaa5e1b34e7e61ae832
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-04-19 22:42:13 +0200

    Second try to remove js folder

--- public/js/components/Component/Component.js
+++
@@ -1,9 +0,0 @@
-export class Component {
-    element;
-    constructor(parentElement, tagName, className) {
-        this.element = document.createElement(tagName);
-        this.element.className = className;
-        parentElement.appendChild(this.element);
-    }
-}
-//# sourceMappingURL=Component.js.map
